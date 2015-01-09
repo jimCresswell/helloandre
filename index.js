77d193347b6b009d5aa1205f2161e5ccfc033b16
jimCresswell <jim.cresswell@gmail.com>
2015-01-09 13:29:39 +0000

    Send a link back to the user.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello Andre!');
+  response.send('<a href="https://github.com/jimCresswell/helloandre">Hello Andre!</a>');
 });
 
 app.listen(app.get('port'), function() {
